922a93d33f8554d3dcd1137b1d7edcf484c0b7aa
akane <akane@maidagency.org>
2024-04-30 17:53:00 +0800

    added experimental heartbeating, waiting for fix

--- src/main/scala/impl/Websocket.scala
+++ src/main/scala/impl/Websocket.scala
@@ -5,18 +5,25 @@ import fabric.io.*
 import fabric.rw.*
 import org.apache.pekko
 import org.apache.pekko.actor.typed.*
+import org.apache.pekko.actor.Cancellable
 import pekko.actor.typed.*
 import pekko.actor.typed.scaladsl.*
+import scala.concurrent.duration.*
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import sttp.client3.*
 import sttp.client3.pekkohttp.PekkoHttpBackend
 import sttp.ws.WebSocket
 
+enum HeartBeatSignal:
+  case Beat
+
 sealed class WebsocketHandler(
-    context: ActorContext[Nothing],
+    context: ActorContext[HeartBeatSignal],
     token: String
-) extends AbstractBehavior[Nothing](context):
+) extends AbstractBehavior[HeartBeatSignal](context):
+
+  var ticker: Option[Cancellable] = None
 
   val backend = PekkoHttpBackend()
   scribe.info("starting up websocket handler")
@@ -26,6 +33,16 @@ sealed class WebsocketHandler(
     .response(asWebSocket(useWebSocket))
     .send(backend)
     .onComplete(_ => backend.close())
+
+  // doesn't work, need fix
+  def startHeartBeat(interval: Int) =
+    scribe.info("heartbeat starting")
+    Behaviors.withTimers[HeartBeatSignal]: timer =>
+        timer.startTimerAtFixedRate(
+          msg = HeartBeatSignal.Beat,
+          interval = 1000.millis
+        )
+        this
 
   def useWebSocket(ws: WebSocket[Future]): Future[Unit] =
     def send(value: Obj) =
@@ -42,29 +59,35 @@ sealed class WebsocketHandler(
 
   private def handleMessage(message: String): Unit =
     import org.maidagency.impl.gateway.{GatewayPayload as Payload, *}
+
     scribe.info(s"got string message: $message")
+
     val json    = JsonParser(message, Format.Json)
     val payload = json.as[Payload]
     payload match
       case Payload(10, Some(HelloPayload(interval, _)), _, _) =>
         scribe.info(s"received heartbeat interval: $interval")
+        startHeartBeat(interval)
       case Payload(11, None, _, _) =>
         scribe.info(s"heartbeat acknowledged")
       case _ =>
         scribe.info(s"received message: $message")
 
-  override def onSignal: PartialFunction[Signal, Behavior[Nothing]] =
+  override def onSignal: PartialFunction[Signal, Behavior[HeartBeatSignal]] =
     case PostStop =>
       scribe.info("killing Websocket handler")
       this
 
-  override def onMessage(msg: Nothing): Behavior[Nothing] =
-    Behaviors.unhandled
+  override def onMessage(msg: HeartBeatSignal): Behavior[HeartBeatSignal] =
+    msg match
+      case HeartBeatSignal.Beat =>
+        scribe.info("got heartbeat")
+        Behaviors.same
 
 end WebsocketHandler
 
 object WebsocketHandler:
-  def apply(token: String): Behavior[Nothing] =
+  def apply(token: String): Behavior[HeartBeatSignal] =
     scribe.info("websocket handler apply method called")
     // Behaviors
     //   .supervise(WebsocketHandler(token))

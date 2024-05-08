c34a06d51c28f119b2614de20419d92215e4f373
SpicyKitten <rathavilash@gmail.com>
2024-05-08 13:59:53 -0500

    added `with` method for readability purposes, manually reformatted gateway intents

--- src/main/scala/impl/websocket/gateway/GatewayIntents.scala
+++ src/main/scala/impl/websocket/gateway/GatewayIntents.scala
@@ -49,6 +49,10 @@ class GatewayIntent(intent: Either[(Int, String), Int]):
 
   def without(other: Int): GatewayIntent = this & ~other
 
+  def `with`(other: GatewayIntent): GatewayIntent = this | other
+
+  def `with`(other: Int): GatewayIntent = this | other
+
   @targetName("not-equal")
   def !=(other: GatewayIntent): Boolean = this != other.toInt
 
@@ -83,57 +87,50 @@ implicit class VectorToIntent(val value: Vector[GatewayIntent]) extends AnyVal:
 
 object GatewayIntent:
   private val INTENTS: mutable.HashMap[Int, String] = new mutable.HashMap()
-  val ALL: GatewayIntent           = GatewayIntent(Right(0x331ffff))
-  val NONE: GatewayIntent          = GatewayIntent(Right(0))
-  val GUILDS: GatewayIntent        = GatewayIntent(Left(0, "GUILDS"))
-  val GUILD_MEMBERS: GatewayIntent = GatewayIntent(Left(1, "GUILD_MEMBERS"))
-  val GUILD_MODERATION: GatewayIntent = GatewayIntent(
-    Left(2, "GUILD_MODERATION")
-  )
-  val GUILD_EMOJIS_AND_STICKERS: GatewayIntent = GatewayIntent(
-    Left(3, "GUILD_EMOJIS_AND_STICKERS")
-  )
-  val GUILD_INTEGRATIONS: GatewayIntent = GatewayIntent(
-    Left(4, "GUILD_INTEGRATIONS")
-  )
-  val GUILD_WEBHOOKS: GatewayIntent = GatewayIntent(Left(5, "GUILD_WEBHOOKS"))
-  val GUILD_INVITES: GatewayIntent  = GatewayIntent(Left(6, "GUILD_INVITES"))
-  val GUILD_VOICE_STATES: GatewayIntent = GatewayIntent(
-    Left(7, "GUILD_VOICE_STATES")
-  )
-  val GUILD_PRESENCES: GatewayIntent = GatewayIntent(Left(8, "GUILD_PRESENCES"))
-  val GUILD_MESSAGES: GatewayIntent  = GatewayIntent(Left(9, "GUILD_MESSAGES"))
-  val GUILD_MESSAGE_REACTIONS: GatewayIntent = GatewayIntent(
-    Left(10, "GUILD_MESSAGE_REACTIONS")
-  )
-  val GUILD_MESSAGE_TYPING: GatewayIntent = GatewayIntent(
-    Left(11, "GUILD_MESSAGE_TYPING")
-  )
-  val DIRECT_MESSAGES: GatewayIntent = GatewayIntent(
-    Left(12, "DIRECT_MESSAGES")
-  )
-  val DIRECT_MESSAGE_REACTIONS: GatewayIntent = GatewayIntent(
-    Left(13, "DIRECT_MESSAGE_REACTIONS")
-  )
-  val DIRECT_MESSAGE_TYPING: GatewayIntent = GatewayIntent(
-    Left(14, "DIRECT_MESSAGE_TYPING")
-  )
-  val MESSAGE_CONTENT: GatewayIntent = GatewayIntent(
-    Left(15, "MESSAGE_CONTENT")
-  )
-  val GUILD_SCHEDULED_EVENTS: GatewayIntent = GatewayIntent(
-    Left(16, "GUILD_SCHEDULED_EVENTS")
-  )
-  val AUTO_MODERATION_CONFIGURATION: GatewayIntent = GatewayIntent(
-    Left(20, "AUTO_MODERATION_CONFIGURATION")
-  )
-  val AUTO_MODERATION_EXECUTION: GatewayIntent = GatewayIntent(
-    Left(21, "AUTO_MODERATION_EXECUTION")
-  )
-  val GUILD_MESSAGE_POLLS: GatewayIntent = GatewayIntent(
-    Left(24, "GUILD_MESSAGE_POLLS")
-  )
-  val DIRECT_MESSAGE_POLLS: GatewayIntent = GatewayIntent(
-    Left(25, "DIRECT_MESSAGE_POLLS")
-  )
+  /* meta intents */
+  val ALL: GatewayIntent  = GatewayIntent(Right(0x331ffff))
+  val NONE: GatewayIntent = GatewayIntent(Right(0))
+  /* individual intents */
+  val GUILDS: GatewayIntent =
+    GatewayIntent(Left(0, "GUILDS"))
+  val GUILD_MEMBERS: GatewayIntent =
+    GatewayIntent(Left(1, "GUILD_MEMBERS"))
+  val GUILD_MODERATION: GatewayIntent =
+    GatewayIntent(Left(2, "GUILD_MODERATION"))
+  val GUILD_EMOJIS_AND_STICKERS: GatewayIntent =
+    GatewayIntent(Left(3, "GUILD_EMOJIS_AND_STICKERS"))
+  val GUILD_INTEGRATIONS: GatewayIntent =
+    GatewayIntent(Left(4, "GUILD_INTEGRATIONS"))
+  val GUILD_WEBHOOKS: GatewayIntent =
+    GatewayIntent(Left(5, "GUILD_WEBHOOKS"))
+  val GUILD_INVITES: GatewayIntent =
+    GatewayIntent(Left(6, "GUILD_INVITES"))
+  val GUILD_VOICE_STATES: GatewayIntent =
+    GatewayIntent(Left(7, "GUILD_VOICE_STATES"))
+  val GUILD_PRESENCES: GatewayIntent =
+    GatewayIntent(Left(8, "GUILD_PRESENCES"))
+  val GUILD_MESSAGES: GatewayIntent =
+    GatewayIntent(Left(9, "GUILD_MESSAGES"))
+  val GUILD_MESSAGE_REACTIONS: GatewayIntent =
+    GatewayIntent(Left(10, "GUILD_MESSAGE_REACTIONS"))
+  val GUILD_MESSAGE_TYPING: GatewayIntent =
+    GatewayIntent(Left(11, "GUILD_MESSAGE_TYPING"))
+  val DIRECT_MESSAGES: GatewayIntent =
+    GatewayIntent(Left(12, "DIRECT_MESSAGES"))
+  val DIRECT_MESSAGE_REACTIONS: GatewayIntent =
+    GatewayIntent(Left(13, "DIRECT_MESSAGE_REACTIONS"))
+  val DIRECT_MESSAGE_TYPING: GatewayIntent =
+    GatewayIntent(Left(14, "DIRECT_MESSAGE_TYPING"))
+  val MESSAGE_CONTENT: GatewayIntent =
+    GatewayIntent(Left(15, "MESSAGE_CONTENT"))
+  val GUILD_SCHEDULED_EVENTS: GatewayIntent =
+    GatewayIntent(Left(16, "GUILD_SCHEDULED_EVENTS"))
+  val AUTO_MODERATION_CONFIGURATION: GatewayIntent =
+    GatewayIntent(Left(20, "AUTO_MODERATION_CONFIGURATION"))
+  val AUTO_MODERATION_EXECUTION: GatewayIntent =
+    GatewayIntent(Left(21, "AUTO_MODERATION_EXECUTION"))
+  val GUILD_MESSAGE_POLLS: GatewayIntent =
+    GatewayIntent(Left(24, "GUILD_MESSAGE_POLLS"))
+  val DIRECT_MESSAGE_POLLS: GatewayIntent =
+    GatewayIntent(Left(25, "DIRECT_MESSAGE_POLLS"))
 end GatewayIntent
